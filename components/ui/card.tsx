a64d017a92e7aa7c0219a7c25755ec2a5b7a7305
shyam-GIIT <shyamkumar890145@gmail.com>
2025-11-10 00:57:26 +0530

    to upload in vercel

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -84,3 +84,4 @@ CardFooter.displayName = "CardFooter"
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
 
 
+
--- components/ui/spline.tsx
+++ components/ui/spline.tsx
@@ -27,3 +27,4 @@ export function SplineScene({ scene, className }: SplineSceneProps) {
 }
 
 
+
